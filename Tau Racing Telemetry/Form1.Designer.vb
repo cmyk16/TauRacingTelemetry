c634613f692fb6acf5b16bbdcaf8f40bf3f1b75c
Will Atherton <willatherton@wills-air.home>
2017-09-01 23:22:59 +0100

    first commit

---
+++ Tau Racing Telemetry/Form1.Designer.vb
@@ -0,0 +1,135 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+	Inherits System.Windows.Forms.Form
+
+	'Form overrides dispose to clean up the component list.
+	<System.Diagnostics.DebuggerNonUserCode()>
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Required by the Windows Form Designer
+	Private components As System.ComponentModel.IContainer
+
+	'NOTE: The following procedure is required by the Windows Form Designer
+	'It can be modified using the Windows Form Designer.  
+	'Do not modify it using the code editor.
+	<System.Diagnostics.DebuggerStepThrough()>
+	Private Sub InitializeComponent()
+		Me.components = New System.ComponentModel.Container()
+		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+		Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+		Me.gear = New System.Windows.Forms.Label()
+		Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+		Me.speed = New System.Windows.Forms.Label()
+		Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.rpm = New System.Windows.Forms.Label()
+		CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+		Me.SuspendLayout()
+		'
+		'PictureBox1
+		'
+		Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+		Me.PictureBox1.Location = New System.Drawing.Point(28, 27)
+		Me.PictureBox1.Name = "PictureBox1"
+		Me.PictureBox1.Size = New System.Drawing.Size(145, 137)
+		Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+		Me.PictureBox1.TabIndex = 0
+		Me.PictureBox1.TabStop = False
+		'
+		'gear
+		'
+		Me.gear.AutoSize = True
+		Me.gear.BackColor = System.Drawing.Color.FromArgb(CType(CType(85, Byte), Integer), CType(CType(85, Byte), Integer), CType(CType(85, Byte), Integer))
+		Me.gear.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.gear.ForeColor = System.Drawing.Color.White
+		Me.gear.Location = New System.Drawing.Point(89, 86)
+		Me.gear.Name = "gear"
+		Me.gear.Size = New System.Drawing.Size(23, 25)
+		Me.gear.TabIndex = 1
+		Me.gear.Text = "0"
+		'
+		'Timer1
+		'
+		Me.Timer1.Enabled = True
+		Me.Timer1.Interval = 500
+		'
+		'speed
+		'
+		Me.speed.AutoSize = True
+		Me.speed.BackColor = System.Drawing.Color.FromArgb(CType(CType(85, Byte), Integer), CType(CType(85, Byte), Integer), CType(CType(85, Byte), Integer))
+		Me.speed.Font = New System.Drawing.Font("Impact", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.speed.ForeColor = System.Drawing.Color.SteelBlue
+		Me.speed.Location = New System.Drawing.Point(85, 126)
+		Me.speed.Name = "speed"
+		Me.speed.Size = New System.Drawing.Size(34, 25)
+		Me.speed.TabIndex = 2
+		Me.speed.Text = "00"
+		Me.speed.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		'
+		'Timer2
+		'
+		Me.Timer2.Enabled = True
+		Me.Timer2.Interval = 250
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.BackColor = System.Drawing.Color.FromArgb(CType(CType(85, Byte), Integer), CType(CType(85, Byte), Integer), CType(CType(85, Byte), Integer))
+		Me.Label1.Font = New System.Drawing.Font("Impact", 6.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Label1.ForeColor = System.Drawing.Color.White
+		Me.Label1.Location = New System.Drawing.Point(90, 114)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(21, 13)
+		Me.Label1.TabIndex = 3
+		Me.Label1.Text = "rpm"
+		Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		'
+		'rpm
+		'
+		Me.rpm.AutoSize = True
+		Me.rpm.BackColor = System.Drawing.Color.FromArgb(CType(CType(85, Byte), Integer), CType(CType(85, Byte), Integer), CType(CType(85, Byte), Integer))
+		Me.rpm.Font = New System.Drawing.Font("Impact", 6.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.rpm.ForeColor = System.Drawing.Color.White
+		Me.rpm.Location = New System.Drawing.Point(87, 106)
+		Me.rpm.Name = "rpm"
+		Me.rpm.Size = New System.Drawing.Size(27, 13)
+		Me.rpm.TabIndex = 4
+		Me.rpm.Text = "0000"
+		Me.rpm.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		'
+		'Form1
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.BackColor = System.Drawing.SystemColors.AppWorkspace
+		Me.ClientSize = New System.Drawing.Size(206, 200)
+		Me.Controls.Add(Me.rpm)
+		Me.Controls.Add(Me.Label1)
+		Me.Controls.Add(Me.speed)
+		Me.Controls.Add(Me.gear)
+		Me.Controls.Add(Me.PictureBox1)
+		Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+		Me.Name = "Form1"
+		Me.Text = "Form1"
+		CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
+
+	End Sub
+
+	Friend WithEvents PictureBox1 As PictureBox
+	Friend WithEvents gear As Label
+	Friend WithEvents Timer1 As Timer
+	Friend WithEvents speed As Label
+	Friend WithEvents Timer2 As Timer
+	Friend WithEvents Label1 As Label
+	Friend WithEvents rpm As Label
+End Class
---
+++ Tau Racing Telemetry/Form1.vb
@@ -0,0 +1,121 @@
+﻿Public Class Form1
+	Dim transparent As Boolean = True
+	Dim image As Boolean = True
+
+	Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+		Me.TransparencyKey = BackColor
+
+
+		SetStyle(ControlStyles.OptimizedDoubleBuffer, True)
+
+	End Sub
+
+	'Declare the variables
+	Dim drag As Boolean = False
+	Dim mousex As Integer
+	Dim mousey As Integer
+
+	Private Sub PictureBox1_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseDown, PictureBox1.MouseDown
+		drag = True 'Sets the variable drag to true.
+		mousex = System.Windows.Forms.Cursor.Position.X - Me.Left 'Sets variable mousex
+		mousey = System.Windows.Forms.Cursor.Position.Y - Me.Top 'Sets variable mousey
+
+	End Sub
+
+	Private Sub PictureBox1_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseMove, PictureBox1.MouseMove
+		'If drag is set to true then move the form accordingly.
+		If drag Then
+			Me.Top = System.Windows.Forms.Cursor.Position.Y - mousey
+			Me.Left = System.Windows.Forms.Cursor.Position.X - mousex
+		End If
+	End Sub
+
+	Private Sub PictureBox1_MouseUp(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseUp, PictureBox1.MouseUp
+		drag = False 'Sets drag to false, so the form does not move according to the code in MouseMove
+	End Sub
+
+	Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+		Dim rnd As New Random()
+		Dim randomNumber As Integer = rnd.Next(0, 7)
+
+		gear.Text = randomNumber.ToString
+	End Sub
+
+	Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+
+		PictureBox1.Refresh()
+
+		Dim rnd As New Random()
+		Dim randomSpeed As Integer = rnd.Next(0, 90)
+		Dim randomRpm As Integer = rnd.Next(0, 10000)
+		Dim randomTps As Integer = rnd.Next(0, 100)
+		Dim randomBps As Integer = rnd.Next(0, 100)
+
+		speed.Text = randomSpeed.ToString
+		rpm.Text = randomRpm.ToString
+
+		Dim g As Drawing.Graphics = PictureBox1.CreateGraphics
+		'Dim brush As New SolidBrush(Color.SteelBlue)
+
+
+		'g.FillPie(brush, 0, 0, 100, 100, 0, randomSpeed)
+
+		Dim SpeedPie = New Rectangle(20, 20, 103, 103) '145,137/ 105,97
+
+		Using gpath As New Drawing2D.GraphicsPath
+			gpath.AddArc(SpeedPie, 120.0!, System.Convert.ToSingle(randomSpeed / 90 * 300))
+			gpath.Widen(New Pen(Color.Empty, 8))
+			g.FillPath(Brushes.SteelBlue, gpath)
+		End Using
+
+		Dim ThrottlePie = New Rectangle(33, 33, 80, 80) '92,83// 80,80
+
+		Using gpath As New Drawing2D.GraphicsPath
+			gpath.AddArc(ThrottlePie, 135.0!, System.Convert.ToSingle(randomTps / 100 * 173))
+			gpath.Widen(New Pen(Color.Empty, 8))
+			g.FillPath(New SolidBrush(Color.FromArgb(128, 0, 255, 0)), gpath)
+		End Using
+
+		Dim BreakPie = New Rectangle(30, 33, 80, 80) '92,83// 80,80
+
+
+		Using gpath As New Drawing2D.GraphicsPath
+			'gpath.AddArc(ThrottlePie, System.Convert.ToSingle(randomBps / 100 * 173), 10.0!)
+			gpath.AddArc(BreakPie, 45, -System.Convert.ToSingle(randomBps / 100 * 95))
+			gpath.Widen(New Pen(Color.Empty, 11))
+			g.FillPath(New SolidBrush(Color.FromArgb(128, 255, 0, 0)), gpath)
+		End Using
+
+	End Sub
+
+	Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+		If e.KeyCode = Keys.P Then
+			If transparent = True Then
+				Me.TransparencyKey = Color.Empty
+				Me.BackColor = Color.Magenta
+				transparent = False
+
+			Else
+				Me.BackColor = SystemColors.AppWorkspace
+				Me.TransparencyKey = BackColor
+				transparent = True
+			End If
+		End If
+
+		If e.KeyCode = Keys.X Then
+			Close()
+		End If
+
+		If e.KeyCode = Keys.I Then
+			If image = True Then
+				PictureBox1.Image = Nothing
+				image = False
+			Else
+				PictureBox1.Image = My.Resources.gauge
+				image = True
+			End If
+
+		End If
+	End Sub
+End Class
